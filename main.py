9f5fab9f782b44d90f8f425b87277675333b1c08
Vlad <vladbashak80@gmail.com>
2025-03-07 21:44:58 +0100

    idk just simple MArokvChainClass

---
+++ main.py
@@ -0,0 +1,40 @@
+# Generator text based on input with Markov Chain
+import random
+class MarkovChain:
+    def __init__(self):
+        self.chain = {} # constructor for init dictionary
+
+    def training(self, text):
+        words = text.split()
+
+        for i in range(len(words) - 1):
+            word = words[i]
+            nextWord = words[i+1]
+
+            if word not in self.chain:
+                self.chain[word] = []
+            self.chain[word].append(nextWord)
+
+    def generation(self, length=10):
+        if not self.chain:
+            print("Chain is not trainer yet! wtf")
+
+        word = random.choice(list(self.chain.keys()))
+        resultArr = [word]
+
+        for _ in range(length - 1):
+            nextWords = self.chain.get(word, [])
+            if not nextWords:
+                break
+            word = random.choice(nextWords)
+            resultArr.append(word)
+
+        return " ".join(resultArr)
+
+text = "сонце світить я гуляю парк гарний я люблю природу"
+gen = MarkovChain()
+gen.training(text)
+
+print(f"Input text: {text} \n")
+for i in range(5):
+ print(f"Generated {i} text: {gen.generation(10)}")
